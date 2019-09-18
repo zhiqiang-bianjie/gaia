2e13ed943eb6d9f27d486a4f3bc4be95391c6f06
Alessio Treglia <alessio@tendermint.com>
2019-09-18 07:59:28 +0100

    WIP

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -8,9 +8,9 @@
 set -euo pipefail
 
 GITIAN_CACHE_DIRNAME='.gitian-builder-cache'
-GO_DEBIAN_RELEASE='1.12.9-1'
-GO_TARBALL="golang-debian-${GO_DEBIAN_RELEASE}.tar.gz"
-GO_TARBALL_URL="https://salsa.debian.org/go-team/compiler/golang/-/archive/debian/${GO_DEBIAN_RELEASE}/${GO_TARBALL}"
+GO_RELEASE='1.13'
+GO_TARBALL="go${GO_RELEASE}.linux-amd64.tar.gz"
+GO_TARBALL_URL="https://dl.google.com/go/${GO_TARBALL}"
 
 # Defaults
 
